193e10fcdbe1957492c9a47c5a8b1162fcdd2a69
Tatiana-100 <izosimovatv100@gmail.com>
2020-02-07 21:36:16 +0300

    My second commit

---
+++ New_file.py
@@ -0,0 +1,19 @@
+# однострочный комментарий
+
+'''
+
+первая строка
+вторая строка
+'''
+
+print('Hello!')
+print('Hello!', 'Mama', 555, sep = 'xxx')
+print('Hello!', 'Mama', 555, end = 'sss')
+
+
+# Ввод
+
+age = input('Input your age')
+
+print(age, type(age))
+print(int(age), type(int(age)))
---
+++ file_1.py
@@ -0,0 +1,23 @@
+# типы переменных
+
+# марка
+name = 'Ford'
+
+print(name, type(name))
+
+# возраст
+age = 3
+
+print(age, type(age))
+
+# объем двигателя
+
+engine_volume = 1.6
+
+print(engine_volume, type(engine_volume))
+
+# есть ли люк
+
+see_sky = False
+
+print(see_sky, type(see_sky))
